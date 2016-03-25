9765a001fd7b17edec1eaccb244e29adb1ac667c
michaeljklein <michael14486@gmail.com>
2016-03-24 23:45:52 -0400

    realizing that this will probably be more than redefining ($) :: PrintArg (a -> b) => (a -> b) -> a -> b

--- src/Play.hs
+++ src/Play.hs
@@ -1,10 +1,19 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE InstanceSigs #-}
 
 import Prelude hiding ((.), ($))
 
 class Functional f where
-  unwrap :: f -> a
-  wrap   :: a -> f
+  getArg :: f -> a -> b
+
+instance Num a => Functional a where
+  getArg x a = []
+
+instance Num a => Functional (a -> a) where
+  -- getArg :: (a -> a) -> b -> c
+  getArg f x = (x :) . getArg (f x)
 
 ($) :: Functional (a -> b) => (a -> b) -> a -> b
 f $ x = f x
