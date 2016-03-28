fbe8e4e71747b355557c569efc28b1f99c2318d1
michaeljklein <michael14486@gmail.com>
2016-03-28 16:58:55 -0400

    apparently needed functional dependencies, as now things are falling into place (second parameter of RFix is begin derived automatically/properly)

--- src/Play.hs
+++ src/Play.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
--- {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE InstanceSigs #-}
 -- {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
 -- {-# LANGUAGE DataKinds #-}
 -- {-# LANGUAGE ConstraintKinds #-}
--- {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FunctionalDependencies #-}
 -- {-# LANGUAGE TypeFamilies #-}
 -- {-# LANGUAGE ImpredicativeTypes #-}
 -- {-# LANGUAGE LiberalTypeSynonyms #-}
@@ -15,7 +15,7 @@
 -- {-# LANGUAGE RankNTypes #-}
 -- {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverlappingInstances #-}
--- {-# LANGUAGE IncoherentInstances #-}
+{-# LANGUAGE IncoherentInstances #-}
 
 data Fix a = Fixed a | Unfixed deriving (Show, Eq)
 
@@ -47,26 +47,35 @@ instance Num a => Num (Fix a) where
 
 
 
-class Fixable a where
-  fixed :: a -> Bool
-  unFix :: (a -> b) -> b
+-- class Fixable a where
+--   fixed :: a -> Bool
+--   unFix :: (a -> b) -> b
 
-instance Fixable (Fix a) where
-  fixed Unfixed = False
-  fixed _       = True
-  unFix f = f Unfixed
+-- instance Fixable (Fix a) where
+--   fixed Unfixed = False
+--   fixed _       = True
+--   unFix f = f Unfixed
 
-instance Fixable a where
-  fixed = const True
-  unFix f = f undefined
+-- instance Fixable a where
+--   fixed = const True
+--   unFix f = f undefined
 
 
 data FWrapper a b = FWrapper a b
+instance Show b => Show (FWrapper a b) where
+  showsPrec n (FWrapper x y) = showsPrec n y
+  show        (FWrapper x y) = show        y
+
+fwrap (x,y) = FWrapper x y
+unwrapF (FWrapper f _) = f
+unwrapC (FWrapper _ c) = c
 
 class Resolution a
 -- instance Resolution b => Resolution (FWrapper a b)
 instance Resolution (FWrapper a b)
 
+isRes :: Resolution a => a -> Bool
+isRes = const True
 
 class Constant a
 instance Eq a => Constant a
@@ -74,43 +83,47 @@ isConstant :: Constant a => a -> a
 isConstant = id
 
 
-class Resolvable a where
-  resolve :: a -> b
-
-class Resolution b => RFix a b where
+class Resolution b => RFix a b | a -> b where
   rfix :: a -> b
 
-instance (Constant a, Resolution b) => RFix (Fix a -> b) b where
-  rfix = unFix
+-- instance Resolution a => RFix a a where
+--   rfix = id
 
-instance (Constant a, RFix b c) => RFix (Fix a -> b) c where
-  rfix = rfix . unFix
+instance Resolution b => RFix (Fix Int -> b) b where
+  rfix = ($ Unfixed)
+
+-- instance (Constant a, RFix b c) => RFix (Fix a -> b) c where
+--   rfix = rfix . unFix
+
+
+tt :: Fix Int -> FWrapper (Int -> String) String
+tt x = fwrap (show, show x)
+
+ttt :: Fix Int -> Fix Int -> FWrapper (Int -> Int -> String) String
+ttt x y = FWrapper (\z w -> show (z + w)) (show (x + y))
 
 
 
+-- ff :: Show a => Fix a -> Fix a -> Fix a -> FWrapper (b -> c -> d -> Bool) String
+-- ff x y z = FWrapper (\x y z -> True) (unwords $ map (++"|") $ map f [x,y,z])
 
+-- gg :: (Show t, Fixable t) => t -> t -> t -> (t -> t -> t -> Bool, String)
+-- gg x y z = (\a b c -> fixed a && fixed b && fixed c, unwords $ map f [x,y,z])
 
-
-ff :: Show a => Fix a -> Fix a -> Fix a -> (b -> c -> d -> Bool, String)
-ff x y z = (\x y z -> True, unwords $ map (++"|") $ map f [x,y,z])
-
-gg :: (Show t, Fixable t) => t -> t -> t -> (t -> t -> t -> Bool, String)
-gg x y z = (\a b c -> fixed a && fixed b && fixed c, unwords $ map f [x,y,z])
-
-f :: (Show a, Fixable a) => a -> String
-f x = if fixed x then show x else "nada"
+-- f :: (Show a, Fixable a) => a -> String
+-- f x = if fixed x then show x else "nada"
 -- f x | fixed x = show $ x
 --     | True    = "nada"
 
-f' :: (Show a, Fixable a) => a -> a -> String
-f' x y = show $ map f [x,y]
+-- f' :: (Show a, Fixable a) => a -> a -> String
+-- f' x y = show $ map f [x,y]
 
-g :: (Show a, Show a1, Show a2, Fixable a, Fixable a1, Fixable a2) => a -> a1 -> a2 -> String
-g x y z = unwords [f x, f y, f z]
+-- g :: (Show a, Show a1, Show a2, Fixable a, Fixable a1, Fixable a2) => a -> a1 -> a2 -> String
+-- g x y z = unwords [f x, f y, f z]
 
-h :: (Num a, Fixable a) => a -> a
-h x | fixed x = x + 1
-    | True    = 1000
+-- h :: (Num a, Fixable a) => a -> a
+-- h x | fixed x = x + 1
+--     | True    = 1000
 
 
 res1 :: (                    Fix a -> t) -> t
@@ -134,8 +147,8 @@ t2 (Fixed x)  _        z _ = (-1) * x
 t2    _      (Fixed y) z _ = (-10) * y
 t2    _       _        _ _ = (-1000)
 
-tt :: (Eq a, Eq b) => Fix a -> Fix b -> (Fix a, Fix b)
-tt x y = (x,y)
+-- tt :: (Eq a, Eq b) => Fix a -> Fix b -> (Fix a, Fix b)
+-- tt x y = (x,y)
 
 
 ----------------------------------------------------------------------------------------------------
